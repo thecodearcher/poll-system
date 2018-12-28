5dde5d82aaf9099938cfcd0e35acada6e6ae562a
thecodearcher <brian.iyoha@gmail.com>
2018-12-28 23:31:42 +0100

    Moved error handler middleware to a module

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
-import path from "path";
 import { userRouter } from "./api/User";
-import { BASE_PATH, ENVIRONMENT, MONGODB_URI } from "./config";
-import { global } from "./middleware";
+import { BASE_PATH, MONGODB_URI } from "./config";
+import { errorHandler, global } from "./middleware";
 import { logger } from "./utils/logger";
+
 class App {
     public express = express();
-    public basePath = BASE_PATH || "";
+    private basePath = BASE_PATH || "";
     constructor() {
         this.boot();
     }
@@ -36,9 +36,9 @@ class App {
         mongoose.connect(MONGODB_URI, {
             useNewUrlParser: true,
         }).then(() => {
-            logger.info("I have successfully connected to the database");
+            logger.info(`Database connection succesful: ${MONGODB_URI}`);
         }).catch((err) => {
-            logger.error(`Something went wrong while i tried connecting to the database→ ${err.message}`);
+            logger.error(`Something went wrong while connecting to the database→ ${err.message}`);
         });
     }
 
@@ -58,36 +58,7 @@ class App {
             process.exit();
         });
 
-        this.express.use((err, req, res, next) => {
-            if (!err.isOperational) {
-                if (ENVIRONMENT !== "development") {
-                    logger.error(
-                        "An unexpected error occurred please restart the application!",
-                        "\nError: " + err.message + " Stack: " + err.stack,
-                    );
-                    process.exit(1);
-                }
-            }
-            logger.error(
-                `${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${
-                req.ip
-                } - Stack: ${err.stack}`,
-            );
-            err.stack = err.stack || "";
-            const errorDetails = {
-                status: false,
-                message: err.message,
-                statusCode: err.statusCode || 500,
-                data: err.data,
-                stack: err.stack,
-            };
-            if (ENVIRONMENT === "production") {
-                delete (errorDetails.stack);
-            }
-
-            res.status(err.statusCode || 500);
-            return res.json(errorDetails);
-        });
+        this.express.use(errorHandler);
 
     }
 }
--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,4 +1,5 @@
+import errorHandler from "./errorHandler";
 import global from "./global";
 import validation from "./validation";
 
-export { global, validation };
+export { global, validation, errorHandler };
