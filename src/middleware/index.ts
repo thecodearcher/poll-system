88d3804b5cb04bd81534ea5a08d2277e8ec34deb
thecodearcher <brian.iyoha@gmail.com>
2018-12-30 22:24:00 +0100

    added passport strategries

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,5 +1,7 @@
 import errorHandler from "./errorHandler";
 import global from "./global";
+
+import { loginStrategy, signupStrategy } from "./passport";
 import validation from "./validation";
 
-export { global, validation, errorHandler };
+export { global, validation, errorHandler, signupStrategy, loginStrategy };
