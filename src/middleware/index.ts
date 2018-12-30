a3be4a479708837eba53c7225601308c9705b4f6
thecodearcher <brian.iyoha@gmail.com>
2018-12-31 00:12:13 +0100

    exported autorization middleware

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,7 +1,7 @@
+import authorized from "./authorization";
 import errorHandler from "./errorHandler";
 import global from "./global";
-
 import { loginStrategy, signupStrategy } from "./passport";
 import validation from "./validation";
 
-export { global, validation, errorHandler, signupStrategy, loginStrategy };
+export { global, validation, errorHandler, signupStrategy, loginStrategy, authorized };
